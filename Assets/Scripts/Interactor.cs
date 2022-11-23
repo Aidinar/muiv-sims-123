6ae7431c96a15fc7493c943e56bb86b0e7971500
Студент <@edumuiv.local>
2022-11-23 19:15:03 +0300

    SDF-Fallback fix

--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -6,7 +6,6 @@ using UnityEngine.Events;
 public class Interactor : MonoBehaviour, IInteractable
 {
     [SerializeField] private string actionText;
-
     [SerializeField] private UnityEvent onInteraction;
 
     public string GetDescription()
