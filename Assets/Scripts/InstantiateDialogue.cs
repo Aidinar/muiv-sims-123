052c440423dc36b62cb4d12e0f51487046a7ea3b
ghMaxi <ec.qwertyui@gmail.com>
2022-11-28 18:12:31 +0300

    Activate function fix

--- Assets/Scripts/InstantiateDialogue.cs
+++ Assets/Scripts/InstantiateDialogue.cs
@@ -4,6 +4,11 @@ using UnityEngine.UI;
 public class InstantiateDialogue : MonoBehaviour
 {
     public GameObject window;
+    public void Activate(string text)
+    {
+        Debug.LogWarning($"Функция Activate устарела!\nВызов: Activate({text});");
+    }
+    
     public void Deactivate()
     {
         window.SetActive(!window.activeSelf);
